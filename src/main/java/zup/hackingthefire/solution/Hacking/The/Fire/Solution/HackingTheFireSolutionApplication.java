0d4cfe759e941d5d8fcfccad82452b103c79faf3
emiteze <emiteze@hotmail.com>
2017-12-01 22:53:39 -0200

    Arquitetura do backend

---
+++ src/main/java/zup/hackingthefire/solution/Hacking/The/Fire/Solution/HackingTheFireSolutionApplication.java
@@ -0,0 +1,12 @@
+package zup.hackingthefire.solution.Hacking.The.Fire.Solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackingTheFireSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackingTheFireSolutionApplication.class, args);
+	}
+}
---
+++ src/test/java/zup/hackingthefire/solution/Hacking/The/Fire/Solution/HackingTheFireSolutionApplicationTests.java
@@ -0,0 +1,16 @@
+package zup.hackingthefire.solution.Hacking.The.Fire.Solution;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HackingTheFireSolutionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
